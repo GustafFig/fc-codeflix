5dc105e84bb6ccfb7bdd5f81e14bd4c53ffebef2
Gustavo Figueiredo <gustavo.figueiredo@fast2mine.com>
2023-06-20 18:26:38 -0300

    add react router dom and basic struct it

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,25 @@
 import { ThemeProvider, Box } from "@mui/system";
+import { Routes, Route, Link } from "react-router-dom";
 import { Header } from "./components/Header";
 import { Layout } from "./components/Layout";
 import { appTheme } from "./config/theme";
+import { Typography } from "@mui/material";
+
+const Home = () => (
+  <Box>
+    <Typography variant="h3" component="h1">
+      Home
+    </Typography>
+  </Box>
+)
+
+const About = () => (
+  <Box>
+    <Typography variant="h3" component="h1">
+      About
+    </Typography>
+  </Box>
+)
 
 function App() {
   return (
@@ -14,7 +32,11 @@ function App() {
       >
         <Header />
         <Layout>
-          
+          <h1>Welcome to Router</h1>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="about" element={<About />} />
+          </Routes>
         </Layout>
       </Box>
     </ThemeProvider>
