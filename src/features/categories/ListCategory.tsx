e63ce337fcc19f755afa21fe40cfdb4faec95a18
Gustavo Figueiredo <gustavo.figueiredo@fast2mine.com>
2023-06-20 19:37:32 -0300

    create category slice with table

--- src/features/categories/ListCategory.tsx
+++ src/features/categories/ListCategory.tsx
@@ -1,9 +1,45 @@
-import { Typography } from '@mui/material'
+import { Box, Button, Typography } from '@mui/material'
+import { selectCategories } from './categorySlice';
+import { useAppSelector } from '../../app/hooks';
+import { Link } from 'react-router-dom';
+import { DataGrid, GridColDef, GridRowsProp } from '@mui/x-data-grid';
 
 export function CategoryList() {
+  const categories = useAppSelector(selectCategories);
+
+  const rows: GridRowsProp = categories.map((category) => ({
+    id: category.id,
+    name: category.name,
+    description: category.description,
+  }));
+
+  const columns: GridColDef[] = [
+    { field: 'id', headerName: 'ID', width: 150 },
+    { field: 'name', headerName: 'NAME', width: 150 },
+    { field: 'description', headerName: 'DESCRIPTION', width: 150 },
+  ];
+
   return (
-    <Typography variant="h3" component="h1">
-      CategoryList
-    </Typography>
+    <Box maxWidth="lg" sx={{ mt: 4, mb: 4 }}>
+      <Box>
+        <Button
+          variant="contained"
+          color="secondary"
+          component={Link}
+          to="/category/create"
+          style={{ marginBottom: "1rem" }}
+        >
+          New Category
+        </Button>
+      </Box>
+
+      {/* {categories.map((category) => (
+        <Typography key={category.id}>{category.name}</Typography>
+      ))} */}
+
+      <div style={{ height: 300, width: '100%' }}>
+        <DataGrid columns={columns} rows={rows} />
+      </div>
+    </Box>
   )
 }
---
+++ src/features/categories/categorySlice.ts
@@ -0,0 +1,45 @@
+import { createSlice } from "@reduxjs/toolkit";
+import { RootState } from "../../app/store";
+
+interface Category {
+  id: string;
+  name: string;
+  description: null | string;
+  is_active: boolean;
+  deleted_at: null | string;
+  created_at: string;
+  updated_at: string;
+}
+
+const category: Category = {
+  id: "8a77241b-5b14-4c61-a0d5-54ac9d993a2c",
+  name: "BlueViolet",
+  description: "LOEREM IPSUM",
+  is_active: true,
+  deleted_at: null,
+  created_at: "2023-06-20T21:51:23+0000",
+  updated_at: "2023-06-20T21:51:23+0000",
+};
+
+export const initialState = [
+  category,
+  { ...category, id: "108475ef-7584-4aa6-8af3-059fb81bef73", name: "Peach" },
+  { ...category, id: "208475ef-7584-4aa6-8af3-059fb81bef73", name: "Apple" },
+  { ...category, id: "308475ef-7584-4aa6-8af3-059fb81bef73", name: "Banana" },
+];
+
+const categoriesSlice = createSlice({
+  name: "categories",
+  initialState,
+  reducers: {
+    createCategory(state, action) {},
+    updateCategory(state, action) {},
+    deleteCategory(state, action) {},
+  },
+});
+
+// Selectors
+
+export const selectCategories = (state: RootState) => state.categories;
+
+export default categoriesSlice.reducer;
