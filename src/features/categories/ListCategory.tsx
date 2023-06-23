0b8385667ba3739daf8cffd655d2c527f7569659
Gustavo Figueiredo <gustavo.figueiredo@fast2mine.com>
2023-06-23 19:29:33 -0300

    feat: add columns with toolbar and edit link at name

--- src/features/categories/ListCategory.tsx
+++ src/features/categories/ListCategory.tsx
@@ -1,8 +1,9 @@
-import { Box, Button, Typography } from '@mui/material'
+import { Box, Button, IconButton, Typography } from '@mui/material'
 import { selectCategories } from './categorySlice';
 import { useAppSelector } from '../../app/hooks';
 import { Link } from 'react-router-dom';
-import { DataGrid, GridColDef, GridRowsProp } from '@mui/x-data-grid';
+import { DataGrid, GridColDef, GridRenderCellParams, GridRowsProp, GridToolbar } from '@mui/x-data-grid';
+import DeleteIcon from "@mui/icons-material/Delete";
 
 export function CategoryList() {
   const categories = useAppSelector(selectCategories);
@@ -11,13 +12,67 @@ export function CategoryList() {
     id: category.id,
     name: category.name,
     description: category.description,
+    isActive: category.is_active,
+    createdAt: new Date(category.created_at).toLocaleDateString('pt-BR'),
   }));
 
   const columns: GridColDef[] = [
-    { field: 'id', headerName: 'ID', width: 150 },
-    { field: 'name', headerName: 'NAME', width: 150 },
-    { field: 'description', headerName: 'DESCRIPTION', width: 150 },
+    {
+      field: 'name',
+      headerName: 'NAME',
+      flex: 1,
+      renderCell: renderNameCell
+    },
+    {
+      field: 'isActive',
+      headerName: 'ATIVO',
+      type: 'boolean',
+      flex: 1,
+      renderCell: renderIsActiveCell,
+    },
+    {
+      field: 'createdAt',
+      headerName: 'Created At',
+      flex: 1,
+    },
+    {
+      field: 'id',
+      headerName: 'action',
+      flex: 1,
+      renderCell: renderActionCell,
+    }
   ];
+
+  function renderNameCell(rowData: GridRenderCellParams) {
+    return (
+      <Link
+        style={{ textDecoration: 'none' }}
+        to={`/categories/edit/${rowData.id}`}
+      >
+        <Typography color="primary">{rowData.value}</Typography>
+      </Link>
+    );
+  }
+
+  function renderActionCell(params: GridRenderCellParams) {
+    return (
+      <IconButton
+        color="secondary"
+        onClick={() => console.log("value")}
+        arial-label="delete"
+      >
+        <DeleteIcon />
+      </IconButton>
+    );
+  }
+
+  function renderIsActiveCell(row: GridRenderCellParams) {
+    return (
+      <Typography color={row.value ? 'primary' : 'secondary'}>
+        {row.value ? 'Ativo' : 'Inativo'}
+      </Typography>
+    );
+  }
 
   return (
     <Box maxWidth="lg" sx={{ mt: 4, mb: 4 }}>
@@ -26,19 +81,27 @@ export function CategoryList() {
           variant="contained"
           color="secondary"
           component={Link}
-          to="/category/create"
+          to="/categories/create"
           style={{ marginBottom: "1rem" }}
         >
           New Category
         </Button>
       </Box>
 
-      {/* {categories.map((category) => (
-        <Typography key={category.id}>{category.name}</Typography>
-      ))} */}
-
       <div style={{ height: 300, width: '100%' }}>
-        <DataGrid columns={columns} rows={rows} />
+        <DataGrid
+          slots={{ toolbar: GridToolbar }}
+          slotProps={{
+            toolbar: {
+              showQuickFilter: true,
+              quickFilterProps: { debounceMs: 500 },
+            }
+          }}
+          disableRowSelectionOnClick={true}
+          pageSizeOptions={[25, 2, 100, 400, 22]}
+          columns={columns}
+          rows={rows}
+        />
       </div>
     </Box>
   )
