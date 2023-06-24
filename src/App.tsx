97f6c03c1885a5268f116114a21c3735a033c10d
Gustavo Figueiredo <gustavo.figueiredo@fast2mine.com>
2023-06-24 14:21:46 -0300

    feat: add notification from notistack

--- src/App.tsx
+++ src/App.tsx
@@ -7,34 +7,39 @@ import { CategoryList } from "./features/categories/ListCategory";
 import { CategoryCreate } from "./features/categories/CreateCategory";
 import { CategoryEdit } from "./features/categories/EditCategory";
 import { Typography } from "@mui/material";
+import { SnackbarProvider } from "notistack";
 
 function App() {
   return (
     <ThemeProvider theme={appTheme}>
-      <Box component="main"
-        sx={{
-          height: "100vh",
-          backgroundColor: (theme) => theme.palette.grey[900],
-        }}
+      <SnackbarProvider
+        maxSnack={3}
+        anchorOrigin={{ vertical: "top", horizontal: "right" }}
       >
-        <Header />
-        <Layout>
-          <h1>Welcome to Router</h1>
-          <Routes>
-            <Route path="/" element={<CategoryList />} />
-            <Route path="/categories" element={<CategoryList />} />
-            <Route path="/categories/create" element={<CategoryCreate />} />
-            <Route path="/categories/edit/:id" element={<CategoryEdit />} />
-
-            <Route path="*" element={
-              <Box sx={{ color: "white" }}>
-                <Typography variant="h1">404</Typography>
-                <Typography variant="h1">Page not found</Typography>
-              </Box>
-            } />
-          </Routes>
-        </Layout>
-      </Box>
+        <Box component="main"
+          sx={{
+            height: "100vh",
+            backgroundColor: (theme) => theme.palette.grey[900],
+          }}
+        >
+          <Header />
+          <Layout>
+            <h1>Welcome to Router</h1>
+            <Routes>
+              <Route path="/" element={<CategoryList />} />
+              <Route path="/categories" element={<CategoryList />} />
+              <Route path="/categories/create" element={<CategoryCreate />} />
+              <Route path="/categories/edit/:id" element={<CategoryEdit />} />
+              <Route path="*" element={
+                <Box sx={{ color: "white" }}>
+                  <Typography variant="h1">404</Typography>
+                  <Typography variant="h1">Page not found</Typography>
+                </Box>
+              } />
+            </Routes>
+          </Layout>
+        </Box>
+      </SnackbarProvider>
     </ThemeProvider>
   );
 }
